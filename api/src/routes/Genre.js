509401e1fa4a74509fdf65a62b067229e243a29c
Facu-J-Boy <boyfacundo45@gmail.com>
2023-01-18 19:59:06 -0300

    order genres and platforms

--- api/src/routes/Genre.js
+++ api/src/routes/Genre.js
@@ -6,7 +6,16 @@ const router = Router();
 router.get('/', async(req, res) => {
     try {
         const genresDB = await genres();
-        res.status(200).send(genresDB);
+        let allGenres = [...new Set(genresDB)].sort(function (a, b) {
+            if (a.name > b.name) {
+              return 1;
+            }
+            if (a.name < b.name) {
+              return -1;
+            }
+            return 0;
+          });
+        res.status(200).send(allGenres);
     } catch (error) {
         res.status(404).send(error.message);
     }
--- api/src/routes/Platform.js
+++ api/src/routes/Platform.js
@@ -4,7 +4,16 @@ const router = Router();
 router.get('/', async(req, res) => {
 try {
     const platformsDB = await platforms();
-        res.status(200).send(platformsDB);
+    let allPlatforms = [...new Set(platformsDB)].sort(function (a, b) {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      });
+        res.status(200).send(allPlatforms);
  } catch (error) {
     res.status(404).send(error.message);
    }
