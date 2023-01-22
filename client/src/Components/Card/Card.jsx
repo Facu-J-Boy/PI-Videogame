5b6918e9be067284e6fabdca853acbc4a21d95aa
Facu-J-Boy <boyfacundo45@gmail.com>
2023-01-22 15:27:40 -0300

    card styles settings

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,17 +1,22 @@
-import './Card.css';
-import React from 'react';
-import {Link} from 'react-router-dom';
+import "./Card.css";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Card = (props) => {
   return (
-  <div className = 'card'>
-     <Link to={`/home/game/${props.id}`}><h3>{props.name}</h3></Link>
-     <img src = {props.background_image} alt={props.name} />
-     <div className = 'genre'>
-      <h3>
-         {props.Genres.join(', ')}
-      </h3>
-     </div>
- </div>
-  )
-}
+    <div className="card">
+      <Link to={`/home/game/${props.id}`} className="cardLink">
+        <h3>{props.name}</h3>
+        <img src={props.background_image} alt={props.name} />
+      </Link>
+      <div className="genreConteiner">
+        {props.Genres.map((gen) => (
+          <li className="genre" key={gen}>
+            {gen}
+          </li>
+        ))}
+      </div>
+      <h5 className="rating">Rating: {props.rating}</h5>
+    </div>
+  );
+};
