0d2e1c050957343d5f11ec1378e4186eabc5cbfd
Facu-J-Boy <boyfacundo45@gmail.com>
2023-01-22 17:15:47 -0300

    ui settings

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
-import { searchVideogame, getAllGenres, getAllPlatforms, filterByGenre, filterByPLatform, OrderByName, OrderByRating, clearVideogames } from '../../redux/Actions';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  searchVideogame,
+  getAllGenres,
+  getAllPlatforms,
+  filterByGenre,
+  filterByPLatform,
+  OrderByName,
+  OrderByRating,
+  clearVideogames,
+} from "../../redux/Actions";
 
-const NavBar = ({setCurrentPage}) => {
-
+const NavBar = ({ setCurrentPage }) => {
   const genres = useSelector((state) => state.genres);
   const platforms = useSelector((state) => state.platforms);
 
   const dispatch = useDispatch();
 
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
   const handleInputChange = (ev) => {
     setName(ev.target.value);
@@ -39,61 +47,73 @@ const NavBar = ({setCurrentPage}) => {
     ev.preventDefault();
     dispatch(clearVideogames());
     dispatch(searchVideogame(name));
-  }
+  };
 
   useEffect(() => {
     dispatch(getAllGenres());
     dispatch(getAllPlatforms());
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-      <label>Search game: </label>
-        <input type='text' value={name} onChange={(ev) => {handleInputChange(ev)}} />
-        <button type='submit' >search</button>
-        </form>
-        <label>Filter by: </label>
-        <label>genres </label>
-        <select onChange={(ev) => {filterGenre(ev)}}>
-          <option>All</option>
+        <label>Search game: </label>
+        <input
+          type="text"
+          value={name}
+          onChange={(ev) => {
+            handleInputChange(ev);
+          }}
+        />
+        <button type="submit">search</button>
+      </form>
+      <label>Filter by: </label>
+      <label>genres </label>
+      <select
+        onChange={(ev) => {
+          filterGenre(ev);
+        }}
+      >
+        <option>All</option>
         {genres?.map((e) => {
-              return (
-                <option
-                key={e.id}
-                value={e.name}
-                >{e.name}</option>
-              )
-            })}
-        </select>
-        <label>platforms </label>
-        <select onChange={(ev) => {filterPlatform(ev)}}>
+          return (
+            <option key={e.id} value={e.name}>
+              {e.name}
+            </option>
+          );
+        })}
+      </select>
+      <label>platforms </label>
+      <select
+        onChange={(ev) => {
+          filterPlatform(ev);
+        }}
+      >
         <option>All</option>
         {platforms?.map((e) => {
-              return (
-                <option
-                key={e.id}
-                value={e.name}
-                >{e.name}</option>
-              )
-            })}
-        </select>
-        <label>Order by: </label>
-        <label>Name </label>
-        <select onChange={(ev) => orderName(ev)} >
-          <option>A-Z</option>
-          <option>Z-A</option>
-        </select>
-        <label>Rating </label>
-        <select onChange={(ev) => orderRating(ev)}>
-          <option> 1-5 </option>
-          <option> 5-1 </option>
-        </select>
-        <Link to={'/home/create'} >
+          return (
+            <option key={e.id} value={e.name}>
+              {e.name}
+            </option>
+          );
+        })}
+      </select>
+      <label>Order by: </label>
+      <label>Name </label>
+      <select onChange={(ev) => orderName(ev)}>
+        <option>A-Z</option>
+        <option>Z-A</option>
+      </select>
+      <label>Rating </label>
+      <select onChange={(ev) => orderRating(ev)}>
+        <option> 1-5 </option>
+        <option> 5-1 </option>
+      </select>
+      <Link to={"/home/create"}>
         <button>Create</button>
-        </Link>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
