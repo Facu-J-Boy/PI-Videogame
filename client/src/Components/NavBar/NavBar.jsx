4bb5bb6de4b18d114b72c39bc0af2e29711c243b
Facu-J-Boy <boyfacundo45@gmail.com>
2023-01-22 22:58:45 -0300

    navBar settings

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -57,88 +57,86 @@ const NavBar = ({ setCurrentPage }) => {
 
   return (
     <div className="label">
-      {/* <form onSubmit={handleSubmit}>
-        <label>Search game: </label>
-        <input
-          type="text"
-          value={name}
-          onChange={(ev) => {
-            handleInputChange(ev);
-          }}
-        />
-        <button type="submit">search</button>
-      </form> */}
-      <div className="box">
-        <form
-          onSubmit={handleSubmit}
-          className="search"
-          action="/search"
-          method="get"
-        >
-          <input
-            className="search-input"
-            autocomplete="off"
-            name="q"
-            placeholder="Search videogame"
-            type="text"
-            value={name}
+      <div className="filterContainer">
+        <div className="box">
+          <form
+            onSubmit={handleSubmit}
+            className="search"
+            action="/search"
+            method="get"
+          >
+            <input
+              className="search-input"
+              autoComplete="off"
+              name="q"
+              placeholder="Search videogame"
+              type="text"
+              value={name}
+              onChange={(ev) => {
+                handleInputChange(ev);
+              }}
+            />
+            <button className="search-button" type="submit">
+              <svg className="search" viewBox="0 0 56.966 56.966">
+                {" "}
+                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path>{" "}
+              </svg>
+            </button>
+          </form>
+        </div>
+        <label>Filter by: genre</label>
+        <div className="content-select">
+          <select
+            aparen="true"
             onChange={(ev) => {
-              handleInputChange(ev);
+              filterGenre(ev);
             }}
-          />
-          <button className="search-button" type="submit">
-            <svg className="search" viewBox="0 0 56.966 56.966">
-              {" "}
-              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path>{" "}
-            </svg>
-          </button>
-        </form>
+          >
+            <option>All</option>
+            {genres?.map((e) => {
+              return (
+                <option key={e.id} value={e.name}>
+                  {e.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <label>platform</label>
+        <div className="content-select">
+          <select
+            onChange={(ev) => {
+              filterPlatform(ev);
+            }}
+          >
+            <option>All</option>
+            {platforms?.map((e) => {
+              return (
+                <option key={e.id} value={e.name}>
+                  {e.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <label>Order by: name</label>
+        <div className="content-select">
+          <select onChange={(ev) => orderName(ev)}>
+            <option>A-Z</option>
+            <option>Z-A</option>
+          </select>
+        </div>
+        <label>rating</label>
+        <div className="content-select">
+          <select onChange={(ev) => orderRating(ev)}>
+            <option> 1-5 </option>
+            <option> 5-1 </option>
+          </select>
+        </div>
+        <Link to={"/home/create"}>
+          <button className="create">Create</button>
+        </Link>
       </div>
-      <label>Filter by: </label>
-      <label>genres </label>
-      <select
-        onChange={(ev) => {
-          filterGenre(ev);
-        }}
-      >
-        <option>All</option>
-        {genres?.map((e) => {
-          return (
-            <option key={e.id} value={e.name}>
-              {e.name}
-            </option>
-          );
-        })}
-      </select>
-      <label>platforms </label>
-      <select
-        onChange={(ev) => {
-          filterPlatform(ev);
-        }}
-      >
-        <option>All</option>
-        {platforms?.map((e) => {
-          return (
-            <option key={e.id} value={e.name}>
-              {e.name}
-            </option>
-          );
-        })}
-      </select>
-      <label>Order by: </label>
-      <label>Name </label>
-      <select onChange={(ev) => orderName(ev)}>
-        <option>A-Z</option>
-        <option>Z-A</option>
-      </select>
-      <label>Rating </label>
-      <select onChange={(ev) => orderRating(ev)}>
-        <option> 1-5 </option>
-        <option> 5-1 </option>
-      </select>
-      <Link to={"/home/create"}>
-        <button>Create</button>
-      </Link>
     </div>
   );
 };
