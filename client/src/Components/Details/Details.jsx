4da08c49a4c424cfecf468c5df8df40777ec022d
Facu-J-Boy <boyfacundo45@gmail.com>
2023-01-24 19:26:20 -0300

    Loading in gameDetail

--- client/src/Components/Details/Details.jsx
+++ client/src/Components/Details/Details.jsx
@@ -2,6 +2,7 @@ import "./Detail.css";
 import React from "react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Loading from "../Loading/Loading";
 import { clearGameDetail, getDetail } from "../../redux/Actions/index.js";
 
 const Details = (props) => {
@@ -17,34 +18,38 @@ const Details = (props) => {
 
   return (
     <div>
-      <div className="content">
-        <div className="detail">
-          <div>
-            <img src={date.background_image} alt={date.name} />
+      {!Object.keys(date).length ? (
+        <Loading />
+      ) : (
+        <div className="content">
+          <div className="detail">
             <div>
-              <h3>Rating:</h3>
-              <p>{date.rating}</p>
+              <img src={date.background_image} alt={date.name} />
+              <div>
+                <h3>Rating:</h3>
+                <p>{date.rating}</p>
+              </div>
+              <div>
+                <h3>Released:</h3>
+                <p>{date.released}</p>
+              </div>
             </div>
-            <div>
-              <h3>Released:</h3>
-              <p>{date.released}</p>
+            <div className="info">
+              <h1>{date.name}</h1>
+              <h4>Description: </h4>
+              {date.description}
+              <h4>Platforms: </h4>
+              {date.Platforms?.map((e) => {
+                return <ul key={e.name}>{e.name}</ul>;
+              })}
+              <h4>Genres: </h4>
+              {date.Genres?.map((e) => {
+                return <ul key={e.name}>{e.name}</ul>;
+              })}
             </div>
-          </div>
-          <div className="info">
-            <h1>{date.name}</h1>
-            <h4>Description: </h4>
-            {date.description}
-            <h4>Platforms: </h4>
-            {date.Platforms?.map((e) => {
-              return <ul key={e.name}>{e.name}</ul>;
-            })}
-            <h4>Genres: </h4>
-            {date.Genres?.map((e) => {
-              return <ul key={e.name}>{e.name}</ul>;
-            })}
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
