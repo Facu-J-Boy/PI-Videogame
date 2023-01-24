259ef338da273214da71e734d70c2c6c8685b941
Facu-J-Boy <boyfacundo45@gmail.com>
2023-01-24 16:08:09 -0300

    detail settings

--- client/src/Components/Details/Details.jsx
+++ client/src/Components/Details/Details.jsx
@@ -1,42 +1,51 @@
-import './Detail.css';
-import React from 'react';
-import { useEffect } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {getDetail} from '../../redux/Actions/index.js';
+import "./Detail.css";
+import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getDetail } from "../../redux/Actions/index.js";
 
 const Details = (props) => {
-
   const date = useSelector((state) => state.gameDetail);
-  console.log('date-->', date);
+  console.log("date-->", date);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch (getDetail(props.match.params.id))
+    dispatch(getDetail(props.match.params.id));
   }, [dispatch, props.match.params.id]);
 
   return (
-    <div className = 'detail'>
-      <h1>{date.name}</h1>
-      <img src={date.background_image} alt={date.name} />
-      <h5>Rating: {date.rating}</h5>
-      <h5>Released: {date.released}</h5>
-      <h5>Description: </h5>
-      {date.description}
-      <h5>Platforms: </h5>
-      {date.Platforms?.map((e) => {
-        return(
-          <ul key={e.name}>{e.name}</ul>
-        )
-      })}
-      <h5>Genres: </h5>
-      {date.Genres?.map((e) => {
-        return(
-          <ul key={e.name}>{e.name}</ul>
-        )
-      })}
+    <div>
+      <div className="content">
+        <div className="detail">
+          <div>
+            <img src={date.background_image} alt={date.name} />
+            <div>
+              <h3>Rating:</h3>
+              <p>{date.rating}</p>
+            </div>
+            <div>
+              <h3>Released:</h3>
+              <p>{date.released}</p>
+            </div>
+          </div>
+          <div className="info">
+            <h1>{date.name}</h1>
+            <h4>Description: </h4>
+            {date.description}
+            <h4>Platforms: </h4>
+            {date.Platforms?.map((e) => {
+              return <ul key={e.name}>{e.name}</ul>;
+            })}
+            <h4>Genres: </h4>
+            {date.Genres?.map((e) => {
+              return <ul key={e.name}>{e.name}</ul>;
+            })}
+          </div>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Details;
