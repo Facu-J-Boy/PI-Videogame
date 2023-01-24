545d56193a22ff706a105f36e03c7a2de7e8c2a8
Facu-J-Boy <boyfacundo45@gmail.com>
2023-01-24 19:11:47 -0300

    scroll to top

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import LandingPage from './Components/LandingPage/LandingPage.jsx';
 import Home from './Components/Home/Home.jsx';
 import Details from './Components/Details/Details.jsx';
 import CreateVideogame from './Components/CreateVideogame/CreateVideogame';
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <Route exact path='/' component={LandingPage} />
       <Route exact path='/home' component={Home} />
       <Route exact path='/home/game/:id' component={Details} />
