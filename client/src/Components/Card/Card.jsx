097364af86247476efae5b5c1022ef9b5d493416
Facu-J-Boy <boyfacundo45@gmail.com>
2023-02-07 22:29:28 -0300

    star in card

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -23,7 +23,24 @@ export const Card = (props) => {
           </li>
         ))}
       </div>
-      <h5 className="rating">Rating: {props.rating}</h5>
+      <div className="rating">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-star"
+          width="15"
+          height="15"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="#ffffff"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+        </svg>
+        <h5 className="number">{props.rating}</h5>
+      </div>
     </div>
   );
 };
